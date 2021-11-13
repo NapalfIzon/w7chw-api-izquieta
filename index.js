9c3eb3dc011dedd3ecb172d43b468f1e49df59df
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-13 13:01:45 +0100

    :arrow_up: Adding initializeServer function

---
+++ index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const initializeMongoDb = require("./database");
+const initializeServer = require("./server");
+
+const port = process.env.PORT || process.env.LOCAL_PORT || 5000;
+
+(async () => {
+  await initializeServer(port);
+  await initializeMongoDb(process.env.MONGODB_STRING);
+})();
---
+++ server/index.js
@@ -0,0 +1,44 @@
+const chalk = require("chalk");
+const debug = require("debug")("socialNetwork:indexServer");
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+const initializeServer = (port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(
+        chalk.bgGreen.red(
+          `Servidor ejecutado OK y escuchando el puerto ${port} ${"ᕦ( ͡° ͜ʖ ͡°)ᕤ"}`
+        )
+      );
+      resolve(server);
+    });
+
+    server.on("error", (error) => {
+      debug(
+        chalk.bgRed.black(
+          `Ha habido un problema inicializando el servidor ಥ╭╮ಥ`
+        )
+      );
+      if (error.code === "EADDRINUSE") {
+        debug(chalk.bgRed.black(`El puerto ${port} está en uso ಥ╭╮ಥ`));
+      }
+      reject();
+    });
+
+    server.on("close", () => {
+      debug(
+        chalk.bgBlue.yellow(
+          `Se ha desconectado el servidor correctamente ( ͡° ͜ʖ ͡°)`
+        )
+      );
+    });
+  });
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+module.exports = initializeServer;
